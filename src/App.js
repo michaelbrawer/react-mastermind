13296ec2f89d17d1dc9105a49c8e069332bad32e
michaelbrawer <michaelbrawer@me.com>
2017-11-28 12:38:28 -0800

    stubs up color picker

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import GameBoard from './components/GameBoard/GameBoard';
+import ColorPicker from './components/ColorPicker/ColorPicker';
 
 class App extends Component {
   render() {
     return (
-      <header className="App-header">React Mastermind</header>
+      <div className="App">
+        <header className="App-header">React Mastermind</header>
+        <GameBoard />
+        <ColorPicker />
+      </div>
     );
   }
 }
