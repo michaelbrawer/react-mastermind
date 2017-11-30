b7c903299bd16c7ffafc3f7c1241c61542bac0ed
michaelbrawer <michaelbrawer@me.com>
2017-11-29 19:01:52 -0800

    adds guess pegs css

--- src/components/GuessRow/GuessRow.jsx
+++ src/components/GuessRow/GuessRow.jsx
@@ -2,18 +2,30 @@ import React from 'react';
 import GuessPegs from '../GuessPegs/GuessPegs';
 import GuessScore from '../GuessScore/GuessScore';
 import ScoreButton from '../ScoreButton/ScoreButton';
-import './GuessRow.css'
+// CSS stylesheets don't export anything, so just import the file:
+import './GuessRow.css';
 
 const GuessRow = (props) => {
   return (
     <div className="GuessRow">
-      <div style={{color: props.currentGuess ? 'black':'lightgrey'}}>
-      {props.rowIdx +1}
+      <div
+        className="GuessRow-Num"
+        style={{color: props.currentGuess ? 'black' : 'lightgrey'}}
+      >
+          {props.rowIdx + 1}
       </div>
-      <GuessPegs code={props.guess.code} colors={props.colors} />
+      <GuessPegs
+        code={props.guess.code}
+        colors={props.colors}
+        currentGuess={props.currentGuess}
+        handlePegClick={props.handlePegClick}
+      />
       {
-        props.currentGuess ?
-          <ScoreButton /> :
+        props.currentGuess && (props.guess.score.perfect !== 4) ?
+          <ScoreButton
+            handleScoreClick={props.handleScoreClick}
+            disabled={props.guess.code.includes(null)}
+          /> :
           <GuessScore score={props.guess.score} />
       }
     </div>
