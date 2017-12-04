4d2779aacf1c18f4b076eb940b164d7987a2c850
michaelbrawer <michaelbrawer@me.com>
2017-12-03 18:54:15 -0800

    clean up new game button

--- src/components/NewGameButton/NewGameButton.jsx
+++ src/components/NewGameButton/NewGameButton.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const NewGameButton = (props) => {
   return (
-    <button  
-      className="btn btn-default" 
-      style={{margin: 10}}
-      onClick={props.handleNewGameClick}
-      
-      >
+    <button
+      className="btn btn-default"
+      style={{
+      margin: 10
+    }}
+      onClick={props.handleNewGameClick}>
       New Game
     </button>
   );
