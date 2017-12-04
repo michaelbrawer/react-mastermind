38e9d26e94e43684a8de21a46556b6df87e742ed
michaelbrawer <michaelbrawer@me.com>
2017-12-03 18:59:40 -0800

    cleanup index js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import ReactDOM from 'react-dom';
 import './index.css';
 
 Array.prototype.last = function(){
