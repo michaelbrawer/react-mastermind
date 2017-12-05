34efa867fc13b4cac2bbd5baa34ea4aa02dacb91
michaelbrawer <michaelbrawer@me.com>
2017-12-05 14:09:02 -0800

    flush out seeds

--- seeds.js
+++ seeds.js
@@ -4,10 +4,10 @@ var Score = require('./models/score');
 
 Score.remove({}).then(() => {
   Score.create([
-    {initials: 'ABC', numGuesses: 8, seconds: 6999},
-    {initials: 'bac', numGuesses: 5, seconds: 3151},
-    {initials: 'XYZ', numGuesses: 8, seconds: 5512},
-    {initials: 'JD', numGuesses: 7, seconds: 4567}
+    {initials: 'MCB', numGuesses: 3, seconds: 1000},
+    {initials: 'CBE', numGuesses: 5, seconds: 2000},
+    {initials: 'XYZ', numGuesses: 8, seconds: 3000},
+    {initials: 'RB', numGuesses: 6, seconds: 4000}
   ]).then(() => {
     process.exit();
   });
